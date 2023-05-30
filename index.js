82c41d4d45492c317b982d85eb7a80545d9b2c1d
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-05-30 09:31:50 +0300

    feat: add Sprite class

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const c = canvas.getContext('2d');
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 9; // 576
 
+class Sprite {
+    constructor({position}) {
+        this.position = position;
+        this.image = new Image();
+        this.image.src = './img/backgroundLevel1.png'
+    }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
+}
+
+const backgroundLevel1 = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    }
+})
+
 const player = new Player();
 const keys = {
     w: {
@@ -23,6 +41,7 @@ function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'white';
     c.fillRect(0, 0, canvas.width, canvas.height);
+    backgroundLevel1.draw();
 
     player.velocity.x = 0;
     if (keys.d.pressed) player.velocity.x = 5 // if there's only one statement, curly braces aren't necessary
@@ -32,35 +51,3 @@ function animate() {
 };
 
 animate();
-
-window.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'w': 
-            if (player.velocity.y === 0) player.velocity.y = -10;
-            
-            break;
-        case 'a': 
-            // move player to the left
-            keys.a.pressed = true;
-            break;
-        case 'd': 
-            // move player to the right
-            keys.d.pressed = true;
-            break;
-        }
-    }
-)
-
-window.addEventListener('keyup', (event) => {
-    switch (event.key) {
-        case 'a': 
-            // move player to the left
-            keys.a.pressed = false;
-            break;
-        case 'd': 
-            // move player to the right
-            keys.d.pressed = false;
-            break;
-        }
-    }
-)
