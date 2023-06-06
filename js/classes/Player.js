46d6521f2cab58a3effec12ae8f7c644a0256840
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-06-06 13:17:04 +0300

    feat: add king img

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,8 +1,8 @@
 
-class Player {
+class Player extends Sprite {
     constructor({
-        collisionBlocks = []
-    }) {
+        collisionBlocks = [], imageSrc, frameRate }) {
+        super({ imageSrc, frameRate })
         this.position = {
             x: 200,
             y: 200
@@ -13,8 +13,6 @@ class Player {
             y: 0
         }
 
-        this.width = 25;
-        this.height = 25;
         this.sides = {
             bottom: this.position.y + this.height
         }
@@ -24,15 +22,32 @@ class Player {
         console.log(this.collisionBlocks);
     }
 
-    draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-
     update() {    
+        // this is the blue box
+        // c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
         this.position.x += this.velocity.x;
         this.checkForHorizontalCollisions();
         this.applyGravity();
+
+        this.hitbox = {
+            position: {
+                x: this.position.x + 58,
+                y: this.position.y + 34
+            },
+            width: 50,
+            height: 53
+        }
+
+        c.fillRect(
+            this.hitbox.position.x, 
+            this.hitbox.position.y, 
+            this.hitbox.width,
+            this.hitbox.height
+
+
+            )
         this.checkForVerticalCollisions();
 
     }
