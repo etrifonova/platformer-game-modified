bb2d171d80f0aa2b546f7455684c3617377029b4
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-06-16 17:07:11 +0300

    feat: comment out audio

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -53,21 +53,21 @@ window.addEventListener('keyup', (event) => {
     }
 )
 
-const button = document.querySelector("#button");
-const icon = document.querySelector("#button > i");
-const audio = document.querySelector("audio");
+// const button = document.querySelector("#button");
+// const icon = document.querySelector("#button > i");
+// const audio = document.querySelector("audio");
 
-button.addEventListener("click", () => {
-  if (audio.paused) {
-    audio.volume = 0.2;
-    audio.play();
-    icon.classList.remove('fa-volume-up');
-    icon.classList.add('fa-volume-mute');
+// button.addEventListener("click", () => {
+//   if (audio.paused) {
+//     audio.volume = 0.2;
+//     audio.play();
+//     icon.classList.remove('fa-volume-up');
+//     icon.classList.add('fa-volume-mute');
     
-  } else {
-    audio.pause();
-    icon.classList.remove('fa-volume-mute');
-    icon.classList.add('fa-volume-up');
-  }
-  button.classList.add("fade");
-});
+//   } else {
+//     audio.pause();
+//     icon.classList.remove('fa-volume-mute');
+//     icon.classList.add('fa-volume-up');
+//   }
+//   button.classList.add("fade");
+// });
